a99c5574388558c95c6bef40606b40cbef7d5685
Max <maxime.abehserra@gmail.com>
2019-04-05 16:30:12 +0200

    second commit with all files

---
+++ memory/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+// import shuffle from 'lodash.shuffle'
+import './App.css'
+
+import Card from './Card';
+import GuessCount from './GuessCount';
+
+class App extends Component {
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
+  render() {
+    const won = new Date().getSeconds() % 2 === 0
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNE !</p> }
+      </div>
+      
+      
+    
+    )
+
+  }
+}
+
+export default App
